e0d6f2559afa2470b1c6ecac4acfb3be0fce54dd
Rajiv Ramaiah <rajivramaiah@me.com>
2016-08-12 23:40:25 -0400

    added user model

--- app/models/user_model.js
+++ app/models/user_model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
 
 // create a schema for posts with a field
 const UserSchema = new Schema({
@@ -9,6 +10,31 @@ const UserSchema = new Schema({
 UserSchema.set('toJSON', {
   virtuals: true,
 });
+
+UserSchema.pre('save', function beforeyYourModelSave(next) {
+
+  // this is a reference to our model
+  // the function runs in some other context so DO NOT bind it
+  const model = this;
+
+  // generate a salt then run callback
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+
+    // hash (encrypt) our password using the salt
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err); }
+
+      // overwrite plain text password with encrypted password
+      user.password = hash;
+      return next();
+    });
+  });
+  // when done run the next callback with no arguments
+  // call next with an error if you encounter one
+  // return next();
+
+)};
 
 // create model class
 const UserModel = mongoose.model('User', UserSchema);
